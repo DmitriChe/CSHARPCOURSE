e7fe35e5c0556eb7d296d882ceae9b7eefec8b60
DmitriChe <juneority@mail.ru>
2023-01-07 17:08:14 +0300

    Решена первая задача: вывод квадрата числа

---
+++ Seminar01/Ex01/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+// int number = Int16(Console.ReadLine());
+int number = new Random().Next(1, 11);
+int result = number * number;
+Console.WriteLine(result);
