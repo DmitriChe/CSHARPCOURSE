c65f87c41ebba8409dd7521c97663d50582bb82b
DmitriChe <juneority@mail.ru>
2023-01-27 03:43:30 +0300

    Решена задача 2 из дз1

---
+++ Homework01/Ex01/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 1: Напишите программу, которая принимает на вход три числа 
+и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+int a = new Random().Next(10);
+int b = new Random().Next(10);
+int c = new Random().Next(10);
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine(a);
+    }
+    else
+    {
+        Console.WriteLine(c);
+    }
+}
+else
+{
+    if (b > c)
+    {
+        Console.WriteLine(b);
+    }
+    else
+    {
+        Console.WriteLine(c);
+    }
+}
+
