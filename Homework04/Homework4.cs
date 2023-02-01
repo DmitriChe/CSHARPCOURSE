3f55cf663338659b64e66644a9ac46012b050ab9
DmitriChe <juneority@mail.ru>
2023-02-01 18:22:41 +0300

    Решена задача 25 из ДЗ 4

---
+++ Homework04/Homework4.cs
@@ -0,0 +1,11 @@
+﻿using static MyLibrary;
+
+public static class Homework04
+{
+    public static void Task25()
+    {
+        int number = GetNumber();
+        int power = GetNumber();
+        Console.WriteLine($"Число {number} в степени {power} равно {MyPow(number, power)}");
+    }
+}
---
+++ Homework04/MyLibrary.cs
@@ -0,0 +1,18 @@
+﻿public static class MyLibrary
+{
+    public static int GetNumber()
+    {
+        Console.WriteLine("Введите целое число: ");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    public static int MyPow(int a, int b)
+    {
+        int result = 1;
+        for (int i = 1; i <= b; i++)
+        {
+            result *= a;
+        }
+        return result;
+    }
+}
