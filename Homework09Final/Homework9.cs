2be03740df43d1254ea5a73695e7f4e2a530a762
DmitriChe <juneority@mail.ru>
2023-02-04 02:35:37 +0300

    Решены три задания итогового проекта по вводному курсу Языки программирования. Добавлена XML документация к методам и классам

---
+++ Homework09Final/Homework9.cs
@@ -0,0 +1,34 @@
+﻿using static MyLibrary;
+
+public static class Homework09
+{
+    /// <summary>
+    /// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+    /// N = 5 -> "5, 4, 3, 2, 1"
+    /// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+    /// </summary>
+    public static void Task64()
+    {
+        Console.Write(PrintNNaturals(GetNaturalNumber()));
+    }
+
+    /// <summary>
+    /// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+    /// M = 1; N = 15 -> 120
+    /// M = 4; N = 8. -> 30
+    /// </summary>
+    public static void Task66()
+    {
+        Console.WriteLine(GetNaturalsSumm(GetNaturalNumber(), GetNaturalNumber()));
+    }
+
+    /// <summary>
+    /// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+    /// m = 2, n = 3 -> A(m,n) = 9
+    /// m = 3, n = 2 -> A(m,n) = 29
+    /// </summary>
+    public static void Task68()
+    {
+        Console.WriteLine(Akkerman(GetNaturalNumber(), GetNaturalNumber()));
+    }
+}
---
+++ Homework09Final/MyLibrary.cs
@@ -0,0 +1,53 @@
+﻿public static class MyLibrary
+{
+
+    /// <summary>
+    /// Получает число из консоли и возврщает его как натуральное
+    /// </summary>
+    /// <returns>Натуральное число, введенное пользователем</returns>
+    public static int GetNaturalNumber()
+    {
+        Console.Write("Введите натуральное число: ");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    /// <summary>
+    /// Возвращает натуральные числа от N до 1
+    /// </summary>
+    /// <param name="num">Натуральное число (начало последовательности)</param>
+    /// <returns>Натуральное число</returns>
+    public static string PrintNNaturals(int num)
+    {
+        if (num == 1) return $"1";
+        else return $"{num} " + PrintNNaturals(num - 1);
+    }
+
+    /// <summary>
+    /// Подсчет суммы натуральных чисел от M до N
+    /// </summary>
+    /// <param name="m">Натуральное число - начало натурального ряда</param>
+    /// <param name="n">Натуральное число - конец натурального ряда</param>
+    /// <returns>Сумма натуральных чисел от M до N</returns>
+    public static int GetNaturalsSumm(int m, int n)
+    {
+        if (m > n)
+        {
+            return GetNaturalsSumm(n, m);
+        }
+        else if (m == n) return m;
+        else return m + GetNaturalsSumm(m + 1, n);
+    }
+
+    /// <summary>
+    /// Функция Аккермана
+    /// </summary>
+    /// <param name="m">Положительное целое</param>
+    /// <param name="n">Положительное целое</param>
+    /// <returns>Целое число, результат вычисления функции Аккермана</returns>
+    public static int Akkerman(int m, int n)
+    {
+        if (m == 0) return (n + 1);
+        else if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+        else return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
+}
