021adc1d677616edec8ba5cbaa7ea40f98658b6c
DmitriChe <juneority@mail.ru>
2023-02-04 04:12:56 +0300

    Решены обе задачи ДЗ 6. Оформлена XML-документация к метода и классам

--- Homework06/Homework6.cs
+++ Homework06/Homework6.cs
@@ -2,13 +2,22 @@
 
 public static class Homework06
 {
+    /// <summary>
+    /// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+    /// 0, 7, 8, -2, -2 -> 2
+    /// 1, -7, 567, 89, 223-> 3
+    /// </summary>
     public static void Task41()
     {
-
+        Console.WriteLine($"Количество чисел больше нуля: {CountPositive(InputInteger(GetInteger("размер массива")))}");
     }
 
+    /// <summary>
+    /// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+    /// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+    /// </summary>
     public static void Task43()
     {
-        
+        PrintXYcoordinates(FindCrossPointCoordinates(GetInteger("k1"), GetInteger("b1"), GetInteger("k2"), GetInteger("b2")));
     }
 }
--- Homework06/MyLibrary.cs
+++ Homework06/MyLibrary.cs
@@ -1,7 +1,85 @@
 ﻿public static class MyLibrary
 {
-    public static int GetInteger()
+    /// <summary>
+    /// Получает число из консоли и возвращает целое
+    /// </summary>
+    /// <param name="name">Смысл вводимого числа</param>
+    /// <returns>Целое число из консоли от пользователя</returns>
+    public static int GetInteger(string name)
     {
-        return 2;
+        Console.Write($"Введите целое число ({name}): ");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    /// <summary>
+    /// Создает массив из N введенных целых чисел
+    /// </summary>
+    /// <param name="size">Размер массива</param>
+    /// <returns>Массив из N введенных целых чисел</returns>
+    public static int[] InputInteger(int size)
+    {
+        int[] array = new int[size];
+        for (int i = 0; i < array.Length; i++)
+        {
+            // Console.Write($"Введите {i + 1}-е целое число массива: ");
+            // array[i] = Convert.ToInt32(Console.ReadLine());
+            array[i] = GetInteger($"{i + 1}-e");
+        }
+        return array;
+    }
+
+    /// <summary>
+    /// Подсчет количества целых чисел больше нуля в массиве
+    /// </summary>
+    /// <param name="array">Входящий массив целых чисел</param>
+    /// <returns>Число положительных целых в массиве</returns>
+    public static int CountPositive(int[] array)
+    {
+        int countPositive = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > 0) countPositive++;
+        }
+        return countPositive;
+    }
+
+    /// <summary>
+    /// Вычисление точки пересечения двух прямых, заданных коэфф. k1, k2 и b1, b2
+    /// </summary>
+    /// <param name="k1">угловой коэфф. k1</param>
+    /// <param name="b1">b1</param>
+    /// <param name="k2">угловой коэфф. k2</param>
+    /// <param name="b2">b2</param>
+    /// <returns>Массив координат точки пересечения 2-х прямых</returns>
+    public static double[] FindCrossPointCoordinates(int k1, int b1, int k2, int b2)
+    {
+        
+        if (k1 == k2)
+        {
+            return new double[1];  // пустой массив, как признак того, что точки пересечения нет
+        }
+        else
+        {
+            double x = (b2 - b1) / (k1 - k2);
+            double y = k1 * x + b1;
+            double[] coords = {x, y};
+            return coords;
+        }
+    }
+
+    /// <summary>
+    /// Вывод координат точки пересечения 2-х прямых, либо сообщения об ее отсутствии
+    /// </summary>
+    /// <param name="coords">Массив с координатами точки персечения 2-х прямых</param>
+    public static void PrintXYcoordinates(double[] coords)
+    {
+        if (coords.Length != 1)
+        {
+            Console.WriteLine($"Точка пересечения прямых [{coords[0]}, {coords[1]}]");
+        }
+        else
+        {
+            Console.WriteLine("Нет точки пересечения");
+        }
     }
 }
